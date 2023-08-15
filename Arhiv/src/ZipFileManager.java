b62baa9cada116a1a4066a396afe4b33c21dc25b
aokll <pastuhov_1992@mail.ru>
2023-08-15 10:15:57 +0300

    Arhiver(2.1)

--- Arhiv/src/ZipFileManager.java
+++ Arhiv/src/ZipFileManager.java
@@ -1,5 +1,9 @@
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
@@ -8,6 +12,22 @@ public class ZipFileManager {
         this.zipFile = zipFile;
     }
     public void createZip(Path source) throws Exception{
+        String srczipFile = source.getFileName().toString();
+        try (
+                ZipOutputStream zos = new ZipOutputStream(Files.newOutputStream(zipFile));
+                InputStream is = Files.newInputStream(source)
+        ) //Создай новый.
+        // поток архива ZipOutputStream используя переменную класса zipFile,
+        // с помощью метода newOutputStream класса Files.{
+        {
+            ZipEntry zipEntry = new ZipEntry(srczipFile);//Создай новый элемент архива ZipEntry
+            zos.putNextEntry(zipEntry);
 
+            byte[] buffer = new byte[1000];
+            while (is.available()>0){
+                int count = is.read(buffer);
+                zos.write(buffer,0,count);
+            }
+        }
     }
 }
